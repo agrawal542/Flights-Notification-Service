dc7ca274ba011c415a11c157340f992c941c173d
Gautam Agrawal <gautamagrawal542@gmail.com>
2025-02-23 23:36:00 +0530

    add-queue-notification

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -2,5 +2,9 @@ const dotenv = require('dotenv')
 dotenv.config();
 
 module.exports = {
-    PORT: process.env.PORT
+    PORT: process.env.PORT,
+    GMAIL_EMAIL: process.env.GMAIL_EMAIL,
+    GMAIL_PASS: process.env.GMAIL_PASS,
+    SEND_EMAIL: process.env.SEND_EMAIL,
+    GMAIL_EMAIL_SERVICE: process.env.GMAIL_EMAIL_SERVICE
 }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
 const express = require('express')
 const apiRoutes = require('./routes/index.js');
 const { ServerConfig, Logger } = require('./config/index.js');
+const amqplib = require('amqplib');
+const { EmailService } = require('./services/index.js');
+
+async function connectQueue() {
+    try {
+        const connection = await amqplib.connect("amqp://localhost");
+        const channel = await connection.createChannel();
+        await channel.assertQueue("flight-notification-queue");
+        channel.consume("flight-notification-queue", async (data) => {
+            const object = JSON.parse(`${Buffer.from(data.content)}`);
+            await EmailService.sendEmail(ServerConfig.SEND_EMAIL, object.recepientEmail, object.subject, object.text);
+            channel.ack(data);
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 const app = express();
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.use("/api", apiRoutes)
 
-// http://localhost:3008/api/v1/test
-// app.get('/api/v1/blogs',(req,res)=>{
-//     res.send({
-//         data :"working"
-//     })
-// })
-
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
     console.log(`Server is running on PORT : ${ServerConfig.PORT}`)
     Logger.info("Succesfully started the server")
-
+    await connectQueue();
 })
 
